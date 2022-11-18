00f56a9b46c95b4b32f29d0fad905f591ee004f3
곽정환 <erickwak2020@gmail.com>
2022-11-18 16:48:21 +0900

    chart sample example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+1. 차트 라이브러리 NPM 설치
+2. 설치된 라이브러리를 import로 APP.vue에서 로딩
+3. mouted() 라이프 사이크 훅에서 차트를 그림
+
 # vue-chart
 
 ## Project setup
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
-    hello
+    <h1>Chart.js</h1>
+    <canvas id="myChart"></canvas>
   </div>
 </template>
 
 <script>
+import {Chart, registerables} from "chart.js";
+
 export default {
   name: 'App',
   components: {
+  },
+  mounted() {
+    const ctx = document.getElementById('myChart');
+    Chart.register(...registerables);
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
 }
 </script>
